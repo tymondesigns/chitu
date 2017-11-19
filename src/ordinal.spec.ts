7c0a94891084bbf561362c11008b3b8e3f86c299
Sean Tymon <tymon148@gmail.com>
2017-11-19 19:28:29 +0000

    fix ordinal

--- src/ordinal.spec.ts
+++ src/ordinal.spec.ts
@@ -1,14 +1,14 @@
 import { ordinal } from './ordinal'
 
 describe('ordinal', () => {
-
-test('it should return the ordinal value for a given number', () => {
-  expect(ordinal(1)).toEqual('1st')
-  expect(ordinal(2)).toEqual('2nd')
-  expect(ordinal(3)).toEqual('3rd')
-  expect(ordinal(4)).toEqual('4th')
-  expect(ordinal(12)).toEqual('12th')
-  expect(ordinal(22)).toEqual('22nd')
-  expect(ordinal(33)).toEqual('33rd')
-  expect(ordinal(101)).toEqual('101st')
+  test('it should return the ordinal value for a given number', () => {
+    expect(ordinal(1)).toEqual('1st')
+    expect(ordinal(2)).toEqual('2nd')
+    expect(ordinal(3)).toEqual('3rd')
+    expect(ordinal(4)).toEqual('4th')
+    expect(ordinal(12)).toEqual('12th')
+    expect(ordinal(22)).toEqual('22nd')
+    expect(ordinal(33)).toEqual('33rd')
+    expect(ordinal(101)).toEqual('101st')
+  })
 })
